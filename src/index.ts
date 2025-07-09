218f2a3eaadcdf2ce2e74f49915833c48224b690
matheuslanduci <matheuslanduci@gmail.com>
2025-07-09 17:08:32 -0300

    add index

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { dynmail } from './dynmail/dynmail'
+export { plunk } from './provider/plunk/plunk'
+export { resend } from './provider/resend/resend'
+export { sender } from './sender/sender'
